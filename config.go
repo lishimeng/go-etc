35f769e4c0af4e796f4587b1b8c8be164203f566
lishimeng <316527907@qq.com>
2020-03-13 16:52:33 +0800

    v1.0.0

---
+++ config.go
@@ -0,0 +1,37 @@
+package etc
+
+import (
+	"errors"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"os"
+	"path/filepath"
+)
+
+func LoadEnvs(configName string, envPath []string, Config interface{}) (f string, err error) {
+
+	for _, path := range envPath {
+		file := fmt.Sprintf("%s/%s", path, configName)
+		f, err = filepath.Abs(file)
+		if err == nil {
+			if _, err = toml.DecodeFile(f, Config); err == nil {
+				return // config file is exist
+			}
+		}
+	}
+	return f, errors.New("can't find config file")
+}
+
+func SaveEnvs(filename string, config interface{}) (err error) {
+
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	enc := toml.NewEncoder(f)
+	err = enc.Encode(&config)
+	return err
+}
